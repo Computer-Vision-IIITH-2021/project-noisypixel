caaa604e2c31e8c4214b21ddef75f03f8d89f200
shubham1810 <shubham.k.dokania@gmail.com>
2021-03-27 11:33:23 +0000

    changes to dataloader to support sampling of points in the point cloud

--- src/dataloader.py
+++ src/dataloader.py
@@ -19,12 +19,13 @@ class OccupancyNetDataset(Dataset):
         Args:
             root_dir (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied
+            num_points (int): Number of points to sample in the object point cloud from the data
                 on a sample.
         """
         self.root_dir = root_dir
         self.transform = transform
-        self.files = []
         self.num_points = num_points
+        self.files = []
         
         for sub in glob.glob(self.root_dir+'/*'):
             self.files.extend(glob.glob(sub+'/*'))
@@ -33,24 +34,30 @@ class OccupancyNetDataset(Dataset):
         return len(self.files)
 
     def __getitem__(self, idx):
-        
+        # Fetch the file path and setup image folder paths
         req_path = self.files[idx]
-        img_folder = req_path + '/img_choy2016'
+        img_folder = os.path.join(req_path, 'img_choy2016')
         img_path = random.choice(glob.glob(img_folder+'/*.jpg'))
-#         image = Image.open(img_path)
+
+        # Load the image with opencv and convert to RGB
         image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
-#         image = np.asarray(image)
-#         image = Image.fromarray(image.astype('uint8'), 'RGB')
         
-        points_path = req_path + '/points.npz'
+        # Load the points data
+        points_path = os.path.join(req_path, 'points.npz')
         data = np.load(points_path)
         
+        # Get the actual point of the object
         points = data['points']
-        
+        # Unpack the occupancies of the object
         occupancies = np.unpackbits(data['occupancies'])
 
-        sample = {'image': image, 'points': points, 'occupancies': occupancies}
+        # Sample n points from the data
+        selected_idx = np.random.permutation(np.arange(points.shape[0]))[:self.num_points]
 
+        # Use only the selected indices and pack everything up in a nice dictionary
+        sample = {'image': image, 'points': points[selected_idx], 'occupancies': occupancies[selected_idx]}
+
+        # Apply any transformation necessary
         if self.transform:
             sample = self.transform(sample)
 

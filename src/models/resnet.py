23f47370ea6b9c63c81d46dfc1ca7f822884a98b
shubham1810 <shubham.k.dokania@gmail.com>
2021-03-28 18:31:15 +0530

    Addition of EfficientNet architectures for image encoder

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -1,36 +1,22 @@
 import torch.nn as nn
 from torchvision import models
 
-def normalize_imagenet(x):
-    ''' Normalize input images according to ImageNet standards.
-    Args:
-        x (tensor): input images
-    '''
-    x = x.clone()
-    x[:, 0] = (x[:, 0] - 0.485) / 0.229
-    x[:, 1] = (x[:, 1] - 0.456) / 0.224
-    x[:, 2] = (x[:, 2] - 0.406) / 0.225
-    return x
 
 class Resnet18(nn.Module):
     ''' ResNet-18 encoder network for image input.
     Args:
         c_dim (int): output dimension of the latent embedding
-        normalize (bool): whether the input images should be normalized
     '''
 
-    def __init__(self, c_dim, normalize=True):
+    def __init__(self, c_dim):
         super().__init__()
-        self.normalize = normalize
         self.features = models.resnet18(pretrained=True)
         self.features.fc = nn.Sequential()
         self.fc = nn.Linear(512, c_dim)
 
     def forward(self, x):
-        if self.normalize:
-            x = normalize_imagenet(x)
-        net = self.features(x)
-        out = self.fc(net)
+        x = self.features(x)
+        out = self.fc(x)
         return out
 
 
@@ -38,19 +24,15 @@ class Resnet50(nn.Module):
     ''' ResNet-50 encoder network.
     Args:
         c_dim (int): output dimension of the latent embedding
-        normalize (bool): whether the input images should be normalized
     '''
 
-    def __init__(self, c_dim, normalize=True):
+    def __init__(self, c_dim):
         super().__init__()
-        self.normalize = normalize
         self.features = models.resnet50(pretrained=True)
         self.features.fc = nn.Sequential()
         self.fc = nn.Linear(2048, c_dim)
 
     def forward(self, x):
-        if self.normalize:
-            x = normalize_imagenet(x)
-        net = self.features(x)
-        out = self.fc(net)
+        x = self.features(x)
+        out = self.fc(x)
         return out

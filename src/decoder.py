1e47501f07034db70dc4451f7f6ee8d91e3ad41b
Madhvi19 <madhvi.panchal@students.iiit.ac.in>
2021-04-01 02:45:17 +0530

    integrated res encoder and res decoder blocks

--- src/decoder.py
+++ src/decoder.py
@@ -102,3 +102,52 @@ class OccupancyNetDatasetHDF(Dataset):
             final_image = self.transform(final_image)
 
         return [final_image, final_points, final_gt]
+
+
+class Resnet50(nn.Module):
+''' ResNet-50 encoder network.
+Args:
+    c_dim (int): output dimension of the latent embedding
+'''
+
+def __init__(self, c_dim):
+    super().__init__()
+    self.features = models.resnet50(pretrained=True)
+    self.features.fc = nn.Sequential()
+    self.fc = nn.Linear(2048, c_dim)
+
+def forward(self, x):
+    x = self.features(x)
+    out = self.fc(x)
+    return out
+
+class ResBlockFC(nn.Module):
+    def __init__(self, in_dim, out_dim=None, h_dim=None):
+        super().__init__()
+        if out_dim is None:
+            out_dim = in_dim
+        if h_dim is None:
+            h_dim = min(in_dim, out_dim)
+        
+        self.fc_0 = nn.Linear(in_dim, h_dim)
+        self.fc_1 = nn.Linear(h_dim, out_dim)
+        self.act = nn.ReLU()
+
+        if in_dim == out_dim:
+            self.skip = None
+        else:
+            self.skip = nn.Linear(in_dim, out_dim, bias=False)
+
+        # Initialize weights to zero
+        nn.init.zeros_(self.fc_1.weight)
+    
+    def forward(self, x):
+        out_0 = self.act(self.fc_0(x))
+        out = self.act(self.fc_1(x))
+
+        if self.skip is not None:
+            x_skip = self.skip(x)
+        else:
+            x_skip = x
+        
+        return x_skip + out

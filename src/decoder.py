2c00cbac4747701e6d4226595c72a6c548d95548
Madhvi19 <madhvi.panchal@students.iiit.ac.in>
2021-04-01 22:43:17 +0530

    added final occ net

--- src/decoder.py
+++ src/decoder.py
@@ -187,3 +187,29 @@ class DecoderFC(nn.Module):
         enc = self.blocks(enc)
         out = self.fc(self.act(enc)).squeeze(-1)
         return out
+
+class OccNetImg(nn.Module):
+    """
+    Wrapper for the overall occupancy network module. This will
+    contain the encoder as well as the decoder and provide functionalities
+    such as extraction of feature, decoding to compute occupancy, and an
+    end-to-end forward pass over the encoder-decoder architectures.
+    """
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+    
+
+    def extract_features(self, x):
+        return self.encoder(x)
+    
+    def forward(self, img, pts):
+        # print(img.shape,  pts.shape)
+        # Compute the image features
+        c = self.extract_features(img)
+
+        # print(c.shape)
+        out = self.decoder(pts, c)
+
+        return out

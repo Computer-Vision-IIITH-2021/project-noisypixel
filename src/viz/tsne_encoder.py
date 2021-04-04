9d6ec1ea543230251ba71d60ad78033688476102
Madhvi19 <madhvi.panchal@students.iiit.ac.in>
2021-04-04 22:26:17 +0530

    Extract network layers

--- src/viz/tsne_encoder.py
+++ src/viz/tsne_encoder.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 import torchvision
+from torchvision import models, transforms
+import torch.nn as nn
 import torchvision.transforms as transforms
 
 transform = transforms.Compose(
@@ -15,3 +17,26 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=2,
                                          shuffle=False, num_workers=2)
+
+
+model = models.resnet101(pretrained=True)
+model_weights = [] 
+conv_layers = [] 
+model_children = list(model.children())
+
+
+counter = 0 
+# append all the conv layers and their respective weights to the list
+for i in range(len(model_children)):
+    if type(model_children[i]) == nn.Conv2d:
+        counter += 1
+        model_weights.append(model_children[i].weight)
+        conv_layers.append(model_children[i])
+    elif type(model_children[i]) == nn.Sequential:
+        for j in range(len(model_children[i])):
+            for child in model_children[i][j].children():
+                if type(child) == nn.Conv2d:
+                    counter += 1
+                    model_weights.append(child.weight)
+                    conv_layers.append(child)
+print(f"Total convolutional layers: {counter}")

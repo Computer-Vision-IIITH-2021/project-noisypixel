3d27e7d76f9a26df9e0115d3b130269e22afe2cd
saiamrit <patnaiksai1234@gmail.com>
2021-04-08 11:21:18 +0530

    camera matrix update

--- src/viz/proj_viz.py
+++ src/viz/proj_viz.py
@@ -3,3 +3,10 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib import transforms, gridspec
 import cv2
+
+cam_path = '/home/saiamrit/Documents/CV Project/data/subset/ShapeNet/02691156/1ac29674746a0fc6b87697d3904b168b/img_choy2016/cameras.npz'
+points_path = '/home/saiamrit/Documents/CV Project/data/subset/ShapeNet/02691156/1ac29674746a0fc6b87697d3904b168b/pointcloud/points.npy'
+img_path = '/home/saiamrit/Documents/CV Project/data/subset/ShapeNet/02691156/1ac29674746a0fc6b87697d3904b168b/img_choy2016'
+
+cameras = np.load(cam_path)
+points = np.load(points_path)[5000:10000]

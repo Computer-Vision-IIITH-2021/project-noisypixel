49c311fc54bc6919e431137efa2259b786c9bb56
Shubham Dokania <shubham.k.dokania@gmail.com>
2021-04-08 18:01:00 +0530

    Add a configuration class for experiment handling

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,19 @@ def count_parameters(network):
     for ix in network.parameters():
         tot += ix.flatten().shape[0]
     print("Parameters: {}M".format(np.round(tot/1e06, 3)))
+    
+
+class Config:
+    def __init__(self):
+        self.c_dim = 128
+        self.h_dim = 128
+        self.p_dim = 3
+        self.data_root = "/content/drive/MyDrive/datasets/hdf_data/"
+        self.batch_size = 64
+        self.output_dir = "/content/drive/MyDrive/datasets/occ_artifacts/"
+        self.exp_name = "initial"
+
+        # optimizer related config
+        self.lr = 3e-04
+
+        os.makedirs(self.output_dir, exist_ok=True)

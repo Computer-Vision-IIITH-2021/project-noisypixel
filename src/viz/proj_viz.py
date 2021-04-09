43ccf05d252e3cdc124ac33e7f4e915d272ba262
saiamrit <patnaiksai1234@gmail.com>
2021-04-09 09:21:18 +0530

    pointcloud viz added

--- src/viz/proj_viz.py
+++ src/viz/proj_viz.py
@@ -9,4 +9,13 @@ points_path = '/home/saiamrit/Documents/CV Project/data/subset/ShapeNet/02691156
 img_path = '/home/saiamrit/Documents/CV Project/data/subset/ShapeNet/02691156/1ac29674746a0fc6b87697d3904b168b/img_choy2016'
 
 cameras = np.load(cam_path)
-points = np.load(points_path)[5000:10000]
+points = np.load(points_path)[3000:5000]
+for i in range(10):
+    fig = plt.figure(figsize=(16, 8))
+    print('Using Camera matrix: world_mat_{}'.format(i))
+    for j in range(points.shape[0]):
+        ax0 = plt.subplot(gs[0])
+        ax0.plot(proj[1], proj[0], 'r*', markersize=3)
+        ax0.set_title("Projected Point Cloud")
+        ax0.axis('off')
+    plt.show()    

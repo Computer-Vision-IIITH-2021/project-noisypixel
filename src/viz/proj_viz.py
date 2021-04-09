60b60b4956fc7bcb38919173513fb06e2b4fb334
saiamrit <patnaiksai1234@gmail.com>
2021-04-09 18:21:18 +0530

    projection functionality added

--- src/viz/proj_viz.py
+++ src/viz/proj_viz.py
@@ -13,7 +13,10 @@ points = np.load(points_path)[3000:5000]
 for i in range(10):
     fig = plt.figure(figsize=(16, 8))
     print('Using Camera matrix: world_mat_{}'.format(i))
+    p = cameras['world_mat_{}'.format(i)]
     for j in range(points.shape[0]):
+    	proj = p @ np.append(points[j],1).T
+        proj = proj/proj[2]
         ax0 = plt.subplot(gs[0])
         ax0.plot(proj[1], proj[0], 'r*', markersize=3)
         ax0.set_title("Projected Point Cloud")

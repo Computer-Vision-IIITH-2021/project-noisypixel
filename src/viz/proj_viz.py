258579feae88b148a49a146803d96a8f8daf3308
saiamrit <patnaiksai1234@gmail.com>
2021-04-12 10:21:18 +0530

    visualization orientation improved

--- src/viz/proj_viz.py
+++ src/viz/proj_viz.py
@@ -11,18 +11,22 @@ img_path = '/home/saiamrit/Documents/CV Project/data/subset/ShapeNet/02691156/1a
 cameras = np.load(cam_path)
 points = np.load(points_path)[3000:5000]
 for i in range(10):
+    gs = gridspec.GridSpec(1, 2, width_ratios=[1, 3]) 
     fig = plt.figure(figsize=(16, 8))
+    print('Loading image: 00{}.jpg'.format(i))
+    img = cv2.imread(os.path.join(img_path,'00{}.jpg'.format(i)))
+    im = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
     print('Using Camera matrix: world_mat_{}'.format(i))
     p = cameras['world_mat_{}'.format(i)]
-    img = cv2.imread(os.path.join(img_path,'00{}.jpg'.format(i)))
     for j in range(points.shape[0]):
-    	proj = p @ np.append(points[j],1).T
+        proj = p @ np.append(points[j],1).T
         proj = proj/proj[2]
         ax0 = plt.subplot(gs[0])
         ax0.plot(proj[1], proj[0], 'r*', markersize=3)
         ax0.set_title("Projected Point Cloud")
         ax0.axis('off')
-
+        
+        ax1 = plt.subplot(gs[1])
         ax1.imshow(im)
         ax1.set_title("Corresponding Image")
         ax1.axis('off')

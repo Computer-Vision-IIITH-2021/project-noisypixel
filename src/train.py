b67574bb05a777e9ebeed4d2118f6573047c1c6e
Shubham Dokania <shubham.k.dokania@gmail.com>
2021-04-14 02:11:45 +0530

    Main training in the train.py script

--- src/train.py
+++ src/train.py
@@ -16,9 +16,43 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
 import torchmetrics
 
 from models import *
 from dataset.dataloader import OccupancyNetDatasetHDF
 from trainer import ONetLit
 from utils import Config, count_parameters
+
+
+if __name__ == "__main__":
+    # Get the model configuration
+    config = Config()
+    
+    # Define the lightning module
+    onet = ONetLit(config)
+    
+    # Initialize tensorboard logger
+    logger = TensorBoardLogger(
+        save_dir=os.getcwd(),
+        version=1,
+        name="lightning_logs"
+    )
+    
+    # Define the trainer object
+    trainer = pl.Trainer(
+        gpus=1,
+        # auto_scale_batch_size='binsearch',
+        logger=logger,
+        min_epochs=1,
+        max_epochs=5,
+        default_root_dir=os.getcwd(),
+        log_every_n_steps=10,
+        progress_bar_refresh_rate=2,
+        # precision=16,
+        # stochastic_weight_avg=True,
+        track_grad_norm=2,
+    )
+    
+    # Start training
+    trainer.fit(onet)

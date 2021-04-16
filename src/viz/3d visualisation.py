08dd20b6c04a1d8fc3d1ecd4dbf41fcf864b9791
Shanthika <shanthika.naik26@gmail.com>
2021-04-16 20:47:12 +0530

    update

--- src/viz/3d visualisation.py
+++ src/viz/3d visualisation.py
@@ -9,58 +9,57 @@ import open3d as o3d
 from open3d import JVisualizer
 
 
-# In[20]:
+# In[14]:
 
 
 #from shapenet dataset
-def visualize(path):
+
+def visualize(path,viz_type = 'pc', num_points = 5000,radii = [0.005, 0.01, 0.02, 0.04]):
+    
     #load point cloud and normals
     pc_xyz = np.load(path+'points.npy')
     pc_normals = np.load(path+'normals.npy')
     
     
-    num_points = 5000
+    #sample points or visualisation
+    
     selected_idx = np.random.permutation(np.arange(pc_xyz.shape[0]))[:num_points]
     pc_xyz = pc_xyz[selected_idx]
     pc_normals = pc_normals[selected_idx]
 
  
+    #create point cloud dataset using open3d
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(pc_xyz)
     pcd.normals = o3d.utility.Vector3dVector(pc_normals)
     pcd.colors = o3d.utility.Vector3dVector(pc_normals)
-# 
-#     pcd.paint_uniform_color([1,1,1])
 
-    #create mesh
-    radii = [0.005, 0.01, 0.02, 0.04]
-    rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
+    if(viz_type=='pc'):
+        #visualize point cloud
+        
+        visualizer = JVisualizer()
+        visualizer.add_geometry(pcd)
+        visualizer.show()
+
+        
+    else:
+        
+        #create mesh
+        rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
+    
+        #visualize mesh
+        o3d.visualization.draw_geometries([rec_mesh])
 
     
-    #visualize point cloud
-    visualizer = JVisualizer()
-    visualizer.add_geometry(pcd)
-    visualizer.show()
     
-    #visualize mesh
-#     o3d.visualization.draw_geometries([rec_mesh])
 
 
-# In[21]:
+# In[15]:
 
 
-filename = '/home/shanthika/Documents/CV/project/subset(1)/subset/ShapeNet/02828884/1b0463c11f3cc1b3601104cd2d998272/pointcloud/'
-visualize(filename)
-
-
-# In[42]:
-
-
-
-
-
-# In[ ]:
-
-
-
+path = '/home/shanthika/Documents/CV/project/subset(1)/subset/ShapeNet/'
+class_id = '02828884/'
+file_id = '1b0463c11f3cc1b3601104cd2d998272/'
+filename = path+class_id+file_id+'pointcloud/'
+visualize(filename,viz_type='mesh')
 
